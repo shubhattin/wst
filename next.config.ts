308db11e45ac2014f34c42514b374ca9e10c30a0
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2026-01-08 18:09:31 +0530

    google cloid run

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactCompiler: true
+  // Enable standalone output for Docker deployment
+  // output: 'standalone'
 };
 
 export default nextConfig;
